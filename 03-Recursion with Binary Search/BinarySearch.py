54beee30682fb186aa73907117a86552badef86c
arhamansari11 <arhamansari454647@gmail.com>
2024-09-24 16:29:55 +0500

    03-Recursion with Binary Search

---
+++ 03-Recursion with Binary Search/BinarySearch.py
@@ -0,0 +1,19 @@
+arr = [1, 2, 3, 4, 5]
+
+l = 0
+r = len(arr) - 1
+key = 4
+
+found = False  
+
+while l <= r: 
+    mid = (l + r) // 2
+    if arr[mid] == key:
+        found = True
+        break  
+    elif arr[mid] > key:
+        r = mid - 1  
+    else:
+        l = mid + 1 
+
+print(found)
