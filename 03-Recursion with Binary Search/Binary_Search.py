95bf0b292bf4964d535d3897fdcea734428b319d
arhamansari11 <arhamansari454647@gmail.com>
2024-09-24 13:19:08 +0500

    03-Recursion with Binary Search

--- 03-Recursion with Binary Search/Binary_Search.py
+++ 03-Recursion with Binary Search/Binary_Search.py
@@ -1,2 +1,19 @@
-# Recursion and Binary Search Advance Implementation
+def issorted(arr, length):
+    # Base Case
+    if length <= 1:  # If the length is 1 or 0, it's sorted
+        return True
+    
+    # If the first element is greater than the second, it's not sorted
+    if arr[0] > arr[1]:
+        return False
+    
+    # Recur for the rest of the array
+    return issorted(arr[1:], length - 1)
 
+arr = [2, 4, 10, 8, 9]
+size = len(arr)
+ans = issorted(arr, size)
+if ans:
+    print("Array is Sorted")
+else:
+    print("Array is not Sorted")
